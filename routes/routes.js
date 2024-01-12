2456ca45e7f294d3777cabeda711f380ba51ba52
yzou235 <yumingzou235@gmail.com>
2024-01-12 18:14:34 -0500

    created index.js homepage and added nav bar

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/users', (req, res) => {
-    res.send("All Users");
-});
+// router.get('/users', (req, res) => {
+//     res.send("All Users");
+// });
+
+router.get("/", (req, res) => {
+    res.send("Home Page");
+})
 
 module.exports = router;
